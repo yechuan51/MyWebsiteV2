b55dda36948a11106987e13a75a5c956a588e81b
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-15 18:56:58 +0800

    Removed default page, added my own.

---
+++ website/src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+const MainHomePage: () => JSX.Element = () => {
+    return (
+        <div>
+            <main>
+                <p>Home page WIP.</p>
+            </main>
+        </div>
+    );
+};
+
+export default MainHomePage;
