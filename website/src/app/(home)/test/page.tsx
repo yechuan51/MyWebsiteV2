a200ad3b599210a64f1698547756541f16a7af9b
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-16 03:34:38 +0800

    Create a page where users can add and get kittys.

--- website/src/app/(home)/test/page.tsx
+++ website/src/app/(home)/test/page.tsx
@@ -1,14 +1,11 @@
 import KittyFinder from "@/components/kittyDb/finder";
+import KittyMaker from "@/components/kittyDb/maker";
 
 export default async function TestPage() {
   return (
     <div className="flex justify-center flex-col items-center">
       <h1 className="text-3xl mt-4">Kitty creator</h1>
-      <input
-        type="text"
-        className="border-solid border-4 border-gray-300 p-1 rounded w-96"
-        placeholder="Enter a name for your kitty"
-      />
+      <KittyMaker />
       <KittyFinder />
     </div>
   );
