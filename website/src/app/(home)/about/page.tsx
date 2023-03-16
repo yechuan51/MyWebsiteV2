7179333f70929642cb2469e94c426930cd0188e5
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-03-17 00:19:29 +0800

    Add react markdown.

--- website/src/app/(home)/about/page.tsx
+++ website/src/app/(home)/about/page.tsx
@@ -1,4 +1,32 @@
-export default function AboutPage() {
-  // For now, just display a dummy about page.
-  return <p>About page WIP.</p>;
+import path from "path";
+import fsPromise from "fs/promises";
+import MarkdownContentComp from "@/components/markdown/markdownContent";
+
+const ABOUT_PAGE_PATH = ["public", "content", "about", "about.md"];
+
+async function getAboutPageMarkdown(
+  filePath: string[]
+): Promise<{ error: any } | { content: string }> {
+  try {
+    const targetPath = path.join(process.cwd(), ...filePath);
+    console.log(targetPath);
+    const data = await fsPromise.readFile(targetPath, "utf-8");
+    return { content: data };
+  } catch (error) {
+    console.error(error);
+    return { error: error };
+  }
+}
+
+export default async function AboutPage() {
+  const data = await getAboutPageMarkdown(ABOUT_PAGE_PATH);
+  if ("error" in data) {
+    throw Error(data.error);
+  }
+  const content = data.content;
+  return (
+    <div>
+      <MarkdownContentComp content={content} />
+    </div>
+  );
 }
